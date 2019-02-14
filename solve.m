a14726f4b9acb8d4937999e31b348eb3d18f8bce
Antonio Vivace <avivace4@gmail.com>
2019-02-14 15:26:12 +0100

    Use MATLAB Profiler to get memory informations

--- solve.m
+++ solve.m
@@ -1,8 +1,11 @@
+profile -memory on
+
+inputMatrix='matrices\non_positive_definite\torso3.mat';
 tic();
 load(inputMatrix);
 loadTime=toc();
 
-[filepath,name,ext] = fileparts(inputMatrix)
+[filepath,name,ext] = fileparts(inputMatrix);
 
 A = Problem.A;
 n=size(A,1);
@@ -11,11 +14,16 @@ xe=ones(n,1);
 tic();
 b=A*xe;
 x = A\b;
+[user,sys] = memory();
+mem = user.MemUsedMATLAB;
+
 solveTime=toc();
 err = norm(x-xe)/norm(xe);
 
-sep = ','
-results = [name sep num2str(err) sep num2str(loadTime) sep num2str(solveTime)]
-disp(results)
+sep = ',';
 
-exit()
+
+profile off
+a = profile('info');
+results = [name sep num2str(err) sep num2str(solveTime) sep num2str(a.FunctionTable(3).PeakMem)];
+disp(results)
